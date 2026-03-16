6fa534499ea15929c566be6e3cfef19963797046
Aitesh <35845814+ajiteshbankulaa@users.noreply.github.com>
2026-03-16 09:21:01 -0400

    Initial commit4

--- intellitoggle-nuxt/components/JoinSection.vue
+++ intellitoggle-nuxt/components/JoinSection.vue
@@ -2,8 +2,7 @@
   <section class="bg-white px-4 md:px-16 2xl:px-29 xl:px-20 pt-32 lg:pt-36 pb-32">
     <div class="bg-white">
       <div
-        style="background-image: url('/figma/Background Pattern.png');"
-        class="2xl:w-[1394px] w-full mx-auto 2xl:aspect-[5/1.37] xl:aspect-[5/1.3] lg:aspect-[5/1.8] sm:aspect-[5/1.8] aspect-[5/6.2] bg-cover bg-center bg-no-repeat bg-[#292462] flex flex-col items-center justify-center lg:gap-6 gap-4 xl:rounded-[12px] lg:rounded-[12px] rounded-[40px] lg:px-8 px-5"
+        class="bg-[url('/figma/Background-Pattern.png')] 2xl:w-[1394px] w-full mx-auto 2xl:aspect-[5/1.37] xl:aspect-[5/1.3] lg:aspect-[5/1.8] sm:aspect-[5/1.8] aspect-[5/6.2] bg-cover bg-center bg-no-repeat bg-[#292462] flex flex-col items-center justify-center lg:gap-6 gap-4 xl:rounded-[12px] lg:rounded-[12px] rounded-[40px] lg:px-8 px-5"
       >
         <h2
           class="lg:w-[605px] sm:w-3/5 w-full mx-auto text-white xl:text-[33px] lg:text-4xl text-[20px] font-semibold text-center font-['Satoshi'] leading-[1.15]"
